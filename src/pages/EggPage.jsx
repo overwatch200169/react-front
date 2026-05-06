834be046c7e0ab9490757ec582c8b41345ff9991
overwatch20019 <1091587398@qq.com>
2026-05-06 16:38:30 +0800

    修复彩蛋页面

--- src/pages/EggPage.jsx
+++ src/pages/EggPage.jsx
@@ -29,7 +29,7 @@ const EggPage = () => {
     if (!dateString) return '-'
     const date = new Date(dateString)
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
-  }
+  
   }
 
   const columns = [
